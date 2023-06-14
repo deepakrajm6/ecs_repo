68a11ede837b967da3c9969d8c31c87104ac4f70
Murugan, Deepak Raj <deepakraj.murugan@hidglobal.com>
2023-06-14 15:09:27 +0530

    Added providers

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   region     = "${var.AWS_REGION}"
   access_key = "${var.ACCESS_KEY}"
